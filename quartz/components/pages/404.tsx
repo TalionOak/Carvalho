ab687966fa7b855e8d1287109419f34016e249e2
Ailton Silva <ailtonsscarvalho@gmail.com>
2025-12-26 19:35:32 -0300

    Quartz sync: Dec 26, 2025, 7:35 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -3,15 +3,14 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
-  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}/Carvalho`)
   const baseDir = url.pathname
 
   return (
     <article class="popover-hint">
       <h1>404</h1>
-      aconteceu um erro dos grandes
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir + "/Carvalho"}>{i18n(cfg.locale).pages.error.home}</a>
+      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
