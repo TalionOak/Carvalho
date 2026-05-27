d84e288a042b7e0e6787152ba393090e2bae5595
Ailton Silva <ailtonsscarvalho@gmail.com>
2026-05-27 03:14:14 -0300

    Quartz sync: May 27, 2026, 3:14 AM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,21 @@
+// If using quartz.ts overrides instead of YAML:
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout()
+export const layout = await loadQuartzLayout({
+    defaults: {
+        afterBody: [
+            ExternalPlugin.Comments({
+                provider: "giscus",
+                options: {
+                    repo: "TalionOak/Carvalho",
+                    repoId: "R_kgDOM-crSQ",
+                    category: "Announcements",
+                    categoryId: "DIC_kwDOM-crSc4C97ff",
+                    lang: "pt",
+                },
+            }),
+        ],
+    },
+})
